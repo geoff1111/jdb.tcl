8216b6606b3a1b10a610b93dec4d33851a0aa6f5
geoff1111 <31835373+geoff1111@users.noreply.github.com>
2025-03-22 13:36:42 +1000

    Update testing.tcl

--- testing.tcl
+++ testing.tcl
@@ -1,14 +1,7 @@
 source [file join [file dirname [info script]] .. jimtcl tcltest.tcl]
 
 needs constraint jim
-# set ::debug(here) to a unique value (numeric or string, etc.) anywhere
-# in your script where you need to be able to break based on a break
-# condition.
-set ::debug(here) {}
-# invoke the jdb.tcl debugger on tclscript and pass a string containing
-# debugger commdands to it. The debugger command string is trimmed to remove
-# whitespace from the start and end overall, and also from the start and
-# end of each line.
+
 proc debug {tclscript debuggercmds} {
   foreach line [split [string trim $debuggercmds] "\n"] {
     if {! [string match #* $line]} {
